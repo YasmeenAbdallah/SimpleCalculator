1350510da18f876f93149ac00380ddef0041ff33
yasmeen abdallah <yasmeenabdallah11898@gmail.com>
2022-09-14 16:21:45 +0200

    add 2 test cases according to  Identity Element theory

--- Controllers/SimpleCalculatorController.cs
+++ Controllers/SimpleCalculatorController.cs
@@ -34,15 +34,47 @@ namespace SimpleCalculator.Controllers
                 CalculationResultsModel obj = new CalculationResultsModel();
                 double num1 = 0;
                 double num2 = 0;
-                if (!double.TryParse(nums.Num1.ToString(), out num1)|| !double.TryParse(nums.Num2.ToString(), out num2))
+                // according to  Identity Element theory 
+                if (String.IsNullOrEmpty(nums.Num1.ToString()) && double.TryParse(nums.Num2.ToString(), out num2) && num2 <= 100000000000 && num2 >= -100000000000)
                 {
-                    return BadRequest("please enter a valid  numbers  ");
+
+                    obj.Summation = "the first num is empty so we treated it as 0 the result :" + num2.ToString();
+                    obj.Subtraction = "the first num is empty so we treated it as 0 the result :" + (-1 * num2).ToString();
+                    obj.Multiplication = "the first num is empty so we treated it as 1 the result :" + num2.ToString();
+
+                    if (num2 == 0)
+                    {
+                        obj.Divivsion = "Cannot divide by zero ";
+                    }
+                    else
+                    {
+                        obj.Divivsion = "the first num is empty so we treated it as 1 the result :" + (1 / num2).ToString();
+                    }
+
+                    return Ok(obj);
                 }
-              
+                if (String.IsNullOrEmpty(nums.Num2.ToString()) && double.TryParse(nums.Num1.ToString(), out num1) && num1 <= 100000000000 && num1 >= -100000000000)
+                {
+
+                    obj.Summation = "the second num is empty so we treated it as 0 the result :" + num1.ToString();
+                    obj.Subtraction = "the second num is empty so we treated it as 0 the result :" + num1.ToString();
+                    obj.Multiplication = "the second num is empty so we treated it as 1 the result :" + num1.ToString();
+                    obj.Divivsion = "the second num is empty so we treated it as 1 the result :" + num1.ToString();
+
+
+                    return Ok(obj);
+                }
                 if (num1 > 100000000000 || num1 < -100000000000 || num2 > 100000000000 || num2 < -100000000000)
                 {
                     return BadRequest("please enter the  numbers in range (-100 billion , 100 billion )  ");
                 }
+                if (!double.TryParse(nums.Num1.ToString(), out num1) || !double.TryParse(nums.Num2.ToString(), out num2))
+                {
+                    return BadRequest("please enter a valid  numbers  ");
+                }
+
+
+               
                 
                 obj.Summation = (_OperationServices.Add(num1, num2)).ToString(); ;
                 obj.Subtraction = (_OperationServices.Subtract(num1, num2)).ToString();
